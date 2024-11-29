49d2229a2a6478a7568729f088ffabdfc2216de9
Dinesh Tyagi <dineshtyagi567@gmail.com>
2024-11-29 10:54:03 +0530

    Optimized changes

--- classData.js
+++ classData.js
@@ -2,151 +2,203 @@ export const schedule = {
     "monday": {
       "slot1": {
         "class": false,
-        "start": "08:30:00"
+        "start": "08:30:00",
+        "end":"10:00:00"
       },
       "slot2": {
         "class": true,
-        "start": "10:00:00"
+        "start": "10:05:00",
+        "end":"11:35:00",
+        "link": "https://meet.google.com/yrx-nrks-bpe"
       },
       "slot3": {
         "class": true,
-        "start": "11:30:00"
+        "start": "11:40:00",
+        "end":"13:10:00",
+        "link":"https://meet.google.com/zzs-piwi-gxm"
       },
       "slot4": {
         "class": false,
-        "start": "13:10:00"
+        "start": "13:15:00",
+        "end":"14:45:00"
       },
       "slot5": {
         "class": true,
-        "start": "14:45:00"
+        "start": "14:50:00",
+        "end":"16:20:00",
+        "link":"https://meet.google.com/xxo-ejnx-iqk"
       },
       "slot6": {
         "class": true,
-        "start": "16:25:00"
+        "start": "16:25:00",
+        "end":"17:55:00",
+        "link":"https://meet.google.com/iuj-jgyy-bxz"
       },
       "slot7": {
         "class": false,
-        "start": "18:00:00"
+        "start": "18:00:00",
+        "end":"19:30:00"
       }
     },
     "tuesday": {
       "slot1": {
         "class": true,
-        "start": "08:30:00"
+        "start": "08:30:00",
+        "end":"10:00:00",
+        "link":"https://meet.google.com/xxo-ejnx-iqk"
       },
       "slot2": {
         "class": false,
-        "start": "10:00:00"
+        "start": "10:00:00",
+        "end":"11:35:00"
       },
       "slot3": {
         "class": false,
-        "start": "11:30:00"
+        "start": "11:40:00",
+        "end":"13:10:00",
       },
       "slot4": {
         "class": true,
-        "start": "13:10:00"
+        "start": "13:15:00",
+        "end":"14:50:00",
+        "link":"https://meet.google.com/eid-vbkb-jsg"
       },
       "slot5": {
         "class": true,
-        "start": "14:45:00"
+        "start": "14:50:00",
+        "end":"16:20:00",
+        "link":"https://meet.google.com/iuj-jgyy-bxz"
       },
       "slot6": {
         "class": false,
-        "start": "16:25:00"
+        "start": "16:25:00",
+        "end":"17:55:00",
       },
       "slot7": {
         "class": true,
-        "start": "18:00:00"
+        "start": "18:00:00",
+        "end":"19:30:00",
+        "link":"https://meet.google.com/yfa-rufn-prb"
       }
     },
     "wednesday": {
       "slot1": {
         "class": false,
-        "start": "08:30:00"
+        "start": "08:30:00",
+        "end":"10:00:00"
       },
       "slot2": {
         "class": true,
-        "start": "10:00:00"
+        "start": "10:00:00",
+        "end":"11:35:00",
+        "link":"https://meet.google.com/yrx-nrks-bpe"
       },
       "slot3": {
         "class": true,
-        "start": "11:30:00"
+        "start": "11:40:00",
+        "end":"13:15:00",
+        "link":"https://meet.google.com/zzs-piwi-gxm"
       },
       "slot4": {
         "class": false,
-        "start": "13:10:00"
+        "start": "13:15:00",
+        "end":"14:45:00",
+        
       },
       "slot5": {
         "class": true,
-        "start": "14:45:00"
+        "start": "14:50:00",
+        "end":"16:20:00",
+        "link":"https://meet.google.com/kkp-xqju-whu"
       },
       "slot6": {
         "class": true,
-        "start": "16:25:00"
+        "start": "16:25:00",
+        "end":"17:55:00",
+        "link":"https://meet.google.com/yfa-rufn-prb"
       },
       "slot7": {
         "class": true,
-        "start": "18:00:00"
+        "start": "18:00:00",
+        "end":"19:30:00",
+        "link":"https://meet.google.com/req-rkvh-tmp"
       }
     },
     "thursday": {
       "slot1": {
         "class": false,
-        "start": "08:30:00"
+        "start": "08:30:00",
+        "end":"10:00:00"
       },
       "slot2": {
         "class": false,
-        "start": "10:00:00"
+        "start": "10:05:00",
+        "end":"11:35:00",
+        "link":"https://meet.google.com/ghu-zjuo-bvy"
       },
       "slot3": {
         "class": false,
-        "start": "11:30:00"
+        "start": "11:40:00",
+        "end":"13:10:00",
       },
       "slot4": {
         "class": true,
-        "start": "13:10:00"
+        "start": "13:15:00",
+        "end":"14:45:00",
+        "link":"https://meet.google.com/eid-vbkb-jsg"
       },
       "slot5": {
         "class": false,
-        "start": "14:45:00"
+        "start": "14:50:00",
+        "end":"16:20:00",
       },
       "slot6": {
         "class": false,
-        "start": "16:25:00"
+        "start": "16:25:00",
+        "end":"17:55:00",
       },
       "slot7": {
         "class": true,
-        "start": "18:00:00"
+        "start": "18:00:00",
+        "end":"19:30:00",
+        "link":"https://meet.google.com/req-rkvh-tmp"
       }
     },
     "friday": {
       "slot1": {
         "class": false,
-        "start": "08:30:00"
+        "start": "08:30:00",
+        "end":"10:00:00"
       },
       "slot2": {
         "class": false,
-        "start": "10:00:00"
+        "start": "10:05:00",
+        "end":"11:35:00"
       },
       "slot3": {
         "class": true,
-        "start": "11:30:00"
+        "start": "11:40:00",
+        "end":"13:10:00",
+        "link":"https://meet.google.com/kkp-xqju-whu"
       },
       "slot4": {
         "class": false,
-        "start": "13:10:00"
+        "start": "13:15:00",
+        "end":"14:45:00",
       },
       "slot5": {
         "class": false,
         "start": "14:45:00"
       },
       "slot6": {
-        "class": false,
-        "start": "16:25:00"
+        "start": "16:25:00",
+        "end":"17:55:00",
       },
       "slot7": {
         "class": true,
-        "start": "18:00:00"
+        "start": "18:00:00",
+        "end":"19:30:00",
+        "link":"https://meet.google.com/yfa-rufn-prb"
       }
     }
     
--- index.js
+++ index.js
@@ -1,74 +1,92 @@
 // Or import puppeteer from 'puppeteer-core';
-// import { schedule } from './classData';
 import puppeteer from "puppeteer";
-const url = "https://meet.google.com/use-hyxf-mva";
+import { schedule } from "./classData.js";
+// const url1 = "https://meet.google.com/wqy-bwvj-aib";
+// const url2 = "https://meet.google.com/rzt-qwgf-gmf"
+// const schedule = {
+//   monday: {
+//     slot1: {
+//       class: false,
+//       start: "08:30:00",
+//       end: "10:00:00",
+//     },
+//     slot2: {
+//       class: true,
+//       start: "21:26:00",
+//       end: "21:30:00",
+//       link:url1
+//     },
+//     slot3: {
+//       class: true,
+//       start: "15:57:00",
+//       end: "21:35:00",
+//       link: url2,
+//     },
+//   },
+// };
 
-const schedule = {
-  sunday: {
-    slot1: {
-      class: false,
-      start: "08:30:00",
-      end: "10:00:00",
-    },
-    slot2: {
-      class: true,
-      start: "10:00:00",
-      end: "11:30:00",
-    },
-    slot3: {
-      class: true,
-      start: "13:45:00",
-      end: "15:55:00",
-      link: url,
-    },
-  },
-};
+var classOnGoing = false;
 
 const joinClass = async (meetingData) => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    executablePath:
-      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-    userDataDir: "./User Data/",
-    args: [
-      "--profile-directory=Default",
-      "--no-sandbox",
-      "--disable-setuid-sandbox", // Select your profile here
-    ],
-  });
+  if(!classOnGoing){
+    const browser = await puppeteer.launch({
+      headless: false,
+      executablePath:
+        "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+      userDataDir: "./User Data/",
+      args: [
+        "--profile-directory=Profile 2",
+        "--no-sandbox",
+        "--disable-setuid-sandbox", // Select your profile here
+      ],
+    });
+  
+    const page = await browser.newPage();
+  
+    async function tryJoinMeeting() {
+      await page.goto(meetingData.link);
+  
+      // Wait for the join button to appear using the full XPath
+      try {
+         //mic
+          await page.waitForSelector("#yDmH0d > c-wiz > div > div > div.TKU8Od > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.Pr6Uwe > div > div",{visible: true, timeout:10000})
+        page.click(
+          "#yDmH0d > c-wiz > div > div > div.TKU8Od > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.Pr6Uwe > div > div"
+        );
+        //camera
+        await page.waitForSelector("#yDmH0d > c-wiz > div > div > div.TKU8Od > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.utiQxe > div > div",{visible: true, timeout:10000})
+        page.click("#yDmH0d > c-wiz > div > div > div.TKU8Od > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.utiQxe > div > div")
+          // join button
+          await page.waitForSelector(
+          "#yDmH0d > c-wiz > div > div > div.TKU8Od > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb",
+          { visible: true, timeout: 500000 }
+        );
+        //how can I improve the code? the script? what else do I want it to achieve? I guess once joined I could alter the time it checks
+        page.click(
+          "#yDmH0d > c-wiz > div > div > div.TKU8Od > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb"
+        );       
 
-  const page = await browser.newPage();
-
-  async function tryJoinMeeting() {
-    await page.goto(meetingData.link);
-
-    // Wait for the join button to appear using the full XPath
-    try {
-      const joinButtonXPath =
-        "#yDmH0d > c-wiz > div > div > div:nth-child(29) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb > div > button";
-      await page.waitForSelector(
-        "#yDmH0d > c-wiz > div > div > div:nth-child(30) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb > div > button",
-        { visible: true, timeout: 500000 }
-      );
-      page.click(
-        "#yDmH0d > c-wiz > div > div > div:nth-child(30) > div.crqnQb > div > div.gAGjv > div.vgJExf > div > div > div.d7iDfe.NONs6c > div.shTJQe > div.jtn8y > div.XCoPyb > div > button"
-      );
-    } catch (error) {
-      console.log("Join button not found:", error.message);
+        classOnGoing = true;
+        
+      } catch (error) {
+        console.log("Join button not found:", error.message);
+      }
     }
+  
+    // Keep trying to join every minute
+    await tryJoinMeeting();
+  
+    const now = new Date();
+    const endTime = new Date(now.toDateString() + " " + meetingData.end);
+    const timeUntilEnd = endTime.getTime() - now.getTime();
+  
+    setTimeout(async () => {
+      console.log("Meeting ended. Closing browser.");
+      classOnGoing = false;
+      await browser.close();
+    }, timeUntilEnd);
   }
-
-  // Keep trying to join every minute
-  await tryJoinMeeting();
-
-  const now = new Date();
-  const endTime = new Date(now.toDateString() + " " + meetingData.end);
-  const timeUntilEnd = endTime.getTime() - now.getTime();
-
-  setTimeout(async () => {
-    console.log("Meeting ended. Closing browser.");
-    await browser.close();
-  }, timeUntilEnd);
+  
 };
 
 const checkAndJoinMeeting = () => {
@@ -108,7 +126,7 @@ const getNextSlotStart = (day, currentSlot) => {
 };
 
 // Run the check every minute
-//setInterval(checkAndJoinMeeting, 60000);
+setInterval(checkAndJoinMeeting, 120000);
 
 // Initial check
 checkAndJoinMeeting();
